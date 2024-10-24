00e4158da91897e7d656ca39f370d2debe96c64f
Ravi Kant <ravikantas.kant81@gmail.com>
2024-10-24 15:10:51 +0530

    API test

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const authRouter = require("./src/api");
-
 app.use('/',(req,res)=>{
     res.send("Welcome to Dev Tinder API")
 })
 
-app.use('/api',authRouter);
+app.get("/login",(req,res)=>{
+    res.send('Login Done')
+})
 
 
 app.listen(7777 ,()=>{
--- src/api/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const authRouter = express.Router();
-
-router.get('/login',(req,res)=>{
-    res.send('Login successful.')
-})
-
-module.exports = authRouter;
