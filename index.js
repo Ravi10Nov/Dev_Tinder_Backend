b4a36cc4564f4389aa99eaae66e02b9c81f3fed3
Ravi Kant <ravikantas.kant81@gmail.com>
2024-10-24 15:17:40 +0530

    API test

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const login = require("./src/api/auth");
+
 // app.use('/',(req,res)=>{
 //     res.send("Welcome to Dev Tinder API")
 // })
 
-app.get("/login",(req,res)=>{
-    res.send('Login Done')
-})
+app.get("/login",)
 
 
 app.listen(7777 ,()=>{
